3f081fdc8cca6e9f7fe634b3e735735014952579
priyaagrawal123 <147872840+priyaagrawal123@users.noreply.github.com>
2024-07-14 23:08:38 +0530

    Add files via upload

---
+++ bitwiseoperators.java
@@ -0,0 +1,10 @@
+public class bitwiseoperators {
+    public static void main(String[] args){
+        System.out.println((3& 5));
+        System.out.println((3 | 5));
+        System.out.println((3 ^ 5));
+        System.out.println((~3));
+        System.out.println((3<<1));
+        System.out.println((3>>1));
+    }
+}
